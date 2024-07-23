0c8a2e251a989b55e3368a546e2e4e2d5f4918b5
camilovegag <camilovegag@unisabana.edu.co>
2024-07-22 20:54:57 -0500

    Add heading1 to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
+import { Heading1 } from '@/components/typography';
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello, Lexicon</h1>
+      <Heading1>Hello, Lexicon</Heading1>
     </div>
   );
 }
